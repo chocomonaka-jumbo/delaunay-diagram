90917613a1a718f1dc0287215a2da1a188660476
satoko hirano <g1920535@is.ocha.ac.jp>
2021-12-09 15:03:53 +0900

    add test

--- test_delaunay2.py
+++ test_delaunay2.py
@@ -5,7 +5,17 @@ import numpy as np
 class TestDelaunayDiagram(unittest.TestCase):
     """test class of delaunay.py
     """
-
+    
+    def test_getRandom2DPoints(self):
+        """test method for getRandom2DPoints"""
+        test_width = 100
+        test_height = 100
+        test_n = 50
+        
+        expected = (test_n, 3)
+        result = delauney2.getRandom2DPoints(test_width, test_height, test_n)
+        self.assetEqual(expected, result.shape)
+    
     def test_get_color(self):
         """test method for get_color
         """
